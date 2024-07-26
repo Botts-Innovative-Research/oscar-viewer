446b5ee1a665ce2fc355013d6b24d3042e025009
Salwa Jeries <annette.jeries@gmail.com>
2024-07-26 13:55:49 -0500

    Added custom colors for highlight

--- src/app/style/theme.ts
+++ src/app/style/theme.ts
@@ -1,8 +1,21 @@
 import { PaletteMode, ThemeOptions } from "@mui/material";
 
+declare module "@mui/material/styles" {
+  interface Palette {
+    errorHighlight: string;
+    secondaryHighlight: string;
+  }
+  interface PaletteOptions {
+    errorHighlight: string;
+    secondaryHighlight: string;
+  }
+}
+
 export const getTheme = (mode: PaletteMode): ThemeOptions => ({
   palette: {
     mode,
+    errorHighlight: "#D32F2F4D",
+    secondaryHighlight: "#9C27B04D",
   },
   typography: {},
   components: {
@@ -12,6 +25,13 @@ export const getTheme = (mode: PaletteMode): ThemeOptions => ({
           ...(ownerState.variant === "outlined" && {
             borderRadius: "10px",
           }),
+        }),
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: ({ ownerState }) => ({
+          borderRadius: "10px",
         }),
       },
     },
