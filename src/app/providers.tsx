650a849787a06081242c2650e45ef2f84bbaa1da
Salwa Jeries <annette.jeries@gmail.com>
2024-08-05 13:41:54 -0500

    Removed app provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,8 +5,6 @@ import { ReactNode, useMemo } from "react";
 import CssBaseline from '@mui/material/CssBaseline';
 import { getTheme } from "@/app/style/theme";
 import "@/app/style/global.css"
-import { Provider } from "react-redux";
-import { appStore } from "@/state/Store";
 
 export default function Providers({ children }: { children: ReactNode }) {
 
@@ -20,11 +18,9 @@ export default function Providers({ children }: { children: ReactNode }) {
   );
 
   return (
-    <Provider store={appStore}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        {children}
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
   );
 }
