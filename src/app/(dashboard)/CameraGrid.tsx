90aa8dd6e25dce7c32a2b235fa3a254949774ffb
Alex Almanza <115671044+earocorn@users.noreply.github.com>
2024-08-22 20:54:23 -0500

    Get rid of debug messages

--- src/app/(dashboard)/CameraGrid.tsx
+++ src/app/(dashboard)/CameraGrid.tsx
@@ -34,43 +34,29 @@ export default function CameraGrid() {
   const dss: Datastream[] = Array.from(useSelector((state: RootState) => state.oshSlice.dataStreams.values()));
 
   const lanes: LaneMeta[] = useSelector(selectLanes);
-  const [videoStreams, setVideoStreams] = useState<Datastream[] | null>(null);
   const [lanesWithVideo, setLanesWithVideo] = useState<LaneWithVideo[] | null>(null);
 
   useEffect(() => {
-    // console.error("Populating lane datastreams");
     if(lanesWithVideo == null || lanesWithVideo.length == 0 && dss.length > 0) {
-      // console.error("Inside population block ")
       let laneData: LaneWithVideo[] = []
+
       lanes.map((lane) => {
         console.log(lane)
         const videoDatastreams = dss.filter((ds) => (lane.systemIds.includes(ds.parentSystemId) && ds.name.includes("Video") && ds.name.includes("Lane")));
         const gammaDatastreams = dss.filter((ds) => (lane.systemIds.includes(ds.parentSystemId) && ds.name.includes("Gamma") && ds.name.includes("Count")));
         const neutronDatastreams = dss.filter((ds)=>(lane.systemIds.includes(ds.parentSystemId) && ds.name.includes("Neutron") && ds.name.includes("Count")));
+        
         const laneWithVideo: LaneWithVideo = {
           laneData: lane,
           videoDatastreams: videoDatastreams,
           gammaDatastream: gammaDatastreams[0],
           neutronDatastream: neutronDatastreams[0]
         };
-        laneData.push(laneWithVideo);
-        console.log("Lane with video:")
-        console.log(dss)
-        console.log(laneWithVideo);
-      });
-      // console.error("Lanes POP");
-      console.error(laneData);
-      setLanesWithVideo(laneData);
-    }
 
-    if(videoStreams == null || videoStreams.length == 0) {
-      const videos = dss.filter((ds) => {
-        console.error(ds.name);
-        return ds.name.includes("Video") && ds.name.includes("Lane");
+        laneData.push(laneWithVideo);
       });
-      // console.error("# of videostreams " + videos.length);
-      setVideoStreams(videos);
-      // console.error("Current videostreams " + videos);
+      
+      setLanesWithVideo(laneData);
     }
   }, [dss]);
 
@@ -87,23 +73,6 @@ export default function CameraGrid() {
     setStartItem(maxItems * (value - 1)); // Set startItem
     setEndItem(maxItems * (value - 1) + maxItems); // Set endItem to offset by maxItems
   };
-
-  // Images for demo camera grid
-  // const demoImages = [
-  //   {src: "/FrontGateLeft.png", name: "Front Gate Left", status: "alarm", id: 1},
-  //   {src: "/FrontGateRight.png", name: "Front Gate Right", status: "fault", id: 2},
-  //   {src: "/FerryPOVExit.png", name: "Ferry POV Exit", status: "none", id: 3},
-  //   {src: "/FerryPOVEntry.png", name: "Ferry POV Entry", status: "none", id: 4},
-  //   {src: "/RearGateLeft.png", name: "Rear Gate Left", status: "none", id: 5},
-  //   {src: "/RearGateRight.png", name: "Rear Gate Right", status: "none", id: 6},
-  //   {src: "/FerryPOVExit.png", name: "Ferry POV Exit", status: "none", id: 7},
-  //   {src: "/FerryPOVExit.png", name: "Ferry POV Exit", status: "none", id: 8},
-  //   {src: "/FerryPOVExit.png", name: "Ferry POV Exit", status: "none", id: 9},
-  //   {src: "/FerryPOVExit.png", name: "Ferry POV Exit", status: "none", id: 10},
-  //   {src: "/FerryPOVExit.png", name: "Ferry POV Exit", status: "none", id: 11},
-  //   {src: "/FerryPOVExit.png", name: "Ferry POV Exit", status: "none", id: 12},
-  //   {src: "/FerryPOVExit.png", name: "Ferry POV Exit", status: "none", id: 12},
-  // ]
 
   return (
     <>
