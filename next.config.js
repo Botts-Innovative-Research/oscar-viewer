6ac1f5074e53aa09530f9bb11064419c73ae54c7
Kalyn Stricklin <kalynstricklin@botts-inc.com>
2024-09-04 13:29:10 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -16,14 +16,17 @@ module.exports = {
         });
         return config;
     },
-    // experimental: {
-    //     missingSuspenseWithCSRBailout: true,
-    // },
+    experimental: {
+        missingSuspenseWithCSRBailout: false,
+    },
     typescript: {
         // !! WARN !!
         // Dangerously allow production builds to successfully complete even if
         // your project has type errors.
         // !! WARN !!
         ignoreBuildErrors: true,
-    }
+    },
+    images: {
+        unoptimized: true,
+    },
 }
