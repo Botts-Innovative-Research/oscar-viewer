5412c84cbbaf54b512c7c113f2dc91d0f08c0eb7
earocorn <amavb789@gmail.com>
2024-10-07 13:44:03 -0500

    add adjudication stuff

---
+++ src/lib/adjudication/models/AdjudicationObservation.ts
@@ -0,0 +1,53 @@
+import { Node } from "@/lib/data/osh/Node"
+
+interface ObservationSMLJson {
+    phenomenonTime: string
+    resultTime: string
+    result: AdjudicationResult
+}
+
+interface AdjudicationResult {
+    username: string
+    feedback: string
+    adjudicationCode: string
+    isotopes: string
+    secondaryInspectionStatus: string
+    filePaths: string
+    occupancyId: string
+    alarmingSystemUid: string
+}
+
+export default class AdjudicationObservation {
+    observation: ObservationSMLJson
+
+    constructor(properties: AdjudicationResult) {
+        const now = new Date().toISOString();
+        this.observation = {
+            phenomenonTime: now,
+            resultTime: now,
+            result: properties
+        };
+    }
+
+    async postObservation(node: Node, datastreamId: string): Promise<void> {
+        try {
+            const endpoint = `${node.getConnectedSystemsEndpoint()}/datastreams/${datastreamId}/observations`
+
+            const response = await fetch(endpoint, {
+                method: "POST",
+                headers: {
+                "Content-Type": "application/json"
+                },
+                body: JSON.stringify(this.observation)
+            });
+        
+            if (!response.ok) {
+                throw new Error(`Error: ${response.statusText}`);
+            }
+        
+            console.log("Adjudication posted successfully");
+            } catch (error) {
+            console.error("Failed to post adjudication", error);
+        }
+    }
+}
