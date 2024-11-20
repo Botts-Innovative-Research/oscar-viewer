785c0e6cd44c50c89389e0b8f062983aa796efda
Kalyn Stricklin <kalynstricklin@botts-inc.com>
2024-11-20 15:02:25 -0600

    Update OSHSlice.tsx

--- src/lib/state/OSHSlice.tsx
+++ src/lib/state/OSHSlice.tsx
@@ -19,11 +19,9 @@ export interface IOSHSlice {
 }
 
 const initialNodeOpts: NodeOptions = {
-    name: "Ian Node",
-    // address: "127.0.0.1",
-    // port: 8282,
-    address: "162.238.96.81",
-    port: 8781,
+    name: "New Node",
+    address: "127.0.0.1",
+    port: 8282,
     oshPathRoot: "/sensorhub",
     sosEndpoint: "/sos",
     csAPIEndpoint: "/api",
