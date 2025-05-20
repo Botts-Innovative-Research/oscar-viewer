b861db1268ab4e6615d1caa67cacd0541f251546
Kalyn Stricklin <kalynstricklin@botts-inc.com>
2025-05-20 10:24:52 -0500

    Update Config.ts

--- src/app/_components/state-manager/Config.ts
+++ src/app/_components/state-manager/Config.ts
@@ -122,7 +122,8 @@ export async function getConfigSystemID(node: INode): Promise<string> {
     let systems = await node.fetchSystems();
     console.log("Systems retrieved:", systems);
 
-    let configSystem = systems.find((system: any) => system.properties?.properties.uid === CONFIG_UID);
+
+    let configSystem = systems?.find((system: any) => system.properties?.properties.uid === CONFIG_UID);
 
     console.log("configSys", configSystem)
 
@@ -320,4 +321,3 @@ export const configDatastreamConstant: any = {
             }
         }
     }
-    
