4d39e7fd85d4acf25142e208afc94f0b31602a5a
kalynstricklin <kalynstricklin@botts-inc.com>
2025-09-11 11:54:03 -0500

    Update page.tsx

--- src/app/report/page.tsx
+++ src/app/report/page.tsx
@@ -6,13 +6,13 @@ import {
     Typography
 } from "@mui/material"
 import React from "react";
-import ReportGenerator from "@/app/_components/reportgen/ReportGenerator";
+import Report from "@/app/_components/reportgen/Report";
 
 export default function ReportViewPage() {
     return (
         <Box>
             <Paper variant='outlined' sx={{width: "100%"}}>
-                <ReportGenerator/>
+                <Report/>
             </Paper>
         </Box>
     )
