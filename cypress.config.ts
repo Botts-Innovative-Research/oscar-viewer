0b15f4f8655ec3a411a87a9824a485ea57e65cdb
kalynstricklin <kalynstricklin@botts-inc.com>
2025-09-23 10:52:45 -0500

    Merge pull request #207 from Botts-Innovative-Research/update-snackbar
    update snackbars

---
+++ cypress.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from "cypress";
+import webpack from "@cypress/webpack-dev-server";
+// @ts-ignore
+import path from 'path'
+
+export default defineConfig({
+    component: {
+        devServer: {
+            framework: "react",
+            bundler: "webpack",
+            webpackConfig: {
+                resolve: {
+                    extensions: [".ts", ".tsx", ".js", ".jsx"],
+                    alias: {
+                        '@': path.resolve(__dirname, './src'),
+                    }
+                },
+                module: {
+                    rules: [
+                        {
+                            test: /\.(ts|tsx|js|jsx)$/,
+                            exclude: /node_modules/,
+                            use: [
+                                {
+                                    loader: "babel-loader",
+                                },
+                            ],
+                        },
+                        {
+                            test: /\.css$/,
+                            use: ["style-loader", "css-loader"],
+                        },
+                    ],
+                },
+            },
+        },
+    },
+
+    e2e: {
+        baseUrl: 'http://localhost:3000',
+        setupNodeEvents(on, config) {
+            // implement node event listeners here
+        },
+        experimentalRunAllSpecs: true,
+        testIsolation: false,
+    },
+});
---
+++ cypress/e2e/ServerPage.cy.tsx
@@ -0,0 +1,85 @@
+
+describe('Servers Page (E2E)', () => {
+    beforeEach(() => {
+        // cy.visit('/servers');
+        cy.visitServerPage();
+    });
+
+    describe('Adding Nodes', () => {
+
+        it('Should successfully add non-local node to the list of ndoes', () => {
+            cy.get('input[name="name"]').clear().type('TEST Non-Local Node');
+            cy.get('[name="name"]').should('have.value','TEST Non-Local Node');
+
+            //TODO: put ip addy here
+            cy.get('input[name="address"]').clear().type('IP ADDRESS');
+            cy.get('[name="address"]').should('have.value', 'IP ADDRESS');
+
+            cy.get('input[name="port"]').clear().type('8282');
+            cy.get('[name="port"]').should('have.value','8282');
+
+            cy.get('input[name="username"]').clear().type('admin');
+            cy.get('[name="username"]').should('have.value','admin');
+
+            cy.get('input[name="password"]').clear().type('oscar');
+            cy.get('[name="password"]').should('have.value','oscar');
+
+
+            cy.contains('button','Add Node').click();
+            cy.get('[id="saveNode-snackbar"]')
+                .should('be.visible')
+                .should('match',/Node is reachable | Node is not reachable. Try again./);
+
+
+            cy.get('[id="saveNode-snackbar"]')
+                .should('be.visible')
+                .should('match',/OSCAR Configuration Saved | Failed to save OSCAR Configuration./);
+
+        })
+    });
+
+    describe('Editing/Removing Nodes', () => {
+        it.skip('Edit existing node displays changes in node list', () => {
+
+            cy.contains('button', 'Edit').first().click();
+
+            cy.contains('Edit Node').should('be.visible');
+
+            cy.get('input[name="name"]').clear().type('Testing Node');
+            cy.get('input[name="address"]').clear().type('localhost');
+            cy.get('input[name="port"]').clear().type('8282');
+            cy.get('input[name="username"]').clear().type('admin');
+            cy.get('input[name="password"]').clear().type('oscar');
+
+
+            cy.contains('button','Save Changes').click();
+
+            cy.get('[id="saveNode-snackbar"]')
+                .should('be.visible')
+                .should('match',/Node is reachable | Node is not reachable. Try again./);
+
+
+            cy.get('[id="saveNode-snackbar"]')
+                .should('be.visible')
+                .should('match',/OSCAR Configuration Saved | Failed to save OSCAR Configuration./);
+        });
+
+        it.skip('deleting node removes it from the list', () => {
+
+        });
+
+        it.skip('Cancel new node', () => {
+
+            // fill out node form
+            cy.get('input[name="name"]').clear().type('Testing Node');
+            cy.get('input[name="address"]').clear().type('localhost');
+            cy.get('input[name="port"]').clear().type('8282');
+            cy.get('input[name="username"]').clear().type('admin');
+            cy.get('input[name="password"]').clear().type('oscar');
+
+
+            cy.contains('button','Cancel').click();
+        });
+    });
+
+});
