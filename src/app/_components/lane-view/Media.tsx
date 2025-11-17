4a185bfa7aad1fff75bb907c33b058b9dc02b091
kalynstricklin <kalynstricklin@botts-inc.com>
2025-11-17 11:20:40 -0600

    Update Media.tsx

--- src/app/_components/lane-view/Media.tsx
+++ src/app/_components/lane-view/Media.tsx
@@ -112,13 +112,13 @@ export default function Media({datasources, currentLane}: {datasources: any, cur
         connectDataSources();
 
         return () => {
-            if (datasources.neutron.isConnected())
+            if (datasources?.neutron?.isConnected())
                 datasources.neutron.disconnect();
 
-            if (datasources.gamma.isConnected())
+            if (datasources?.gamma?.isConnected())
                 datasources.gamma.disconnect();
 
-            if (datasources.threshold.isConnected())
+            if (datasources?.threshold?.isConnected())
                 datasources.threshold.disconnect();
         }
 
