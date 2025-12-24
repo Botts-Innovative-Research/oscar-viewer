82ccd6191c302c5e5abc53e6a917af02ec2af541
kalynstricklin <kalynstricklin@botts-inc.com>
2025-12-23 19:40:56 -0600

    added qr code

--- src/app/_components/adjudication/AdjudicationDetail.tsx
+++ src/app/_components/adjudication/AdjudicationDetail.tsx
@@ -13,10 +13,11 @@ import {
     Button,
     Paper,
     Snackbar,
-    TextField, FormControlLabel, Checkbox
+    TextField, FormControlLabel, Checkbox, DialogTitle,
+    Dialog
 
 } from "@mui/material";
-import React, {ChangeEvent, useContext, useEffect, useRef, useState} from "react";
+import React, {ChangeEvent, useCallback, useContext, useEffect, useRef, useState} from "react";
 import AdjudicationLog from "./AdjudicationLog"
 import {EventTableData} from "@/lib/data/oscar/TableHelpers";
 import {AdjudicationCode, AdjudicationCodes} from "@/lib/data/oscar/adjudication/models/AdjudicationConstants";
@@ -37,6 +38,9 @@ import DeleteOutline from "@mui/icons-material/DeleteOutline"
 import {setAdjudicatedEventId, setSelectedEvent} from "@/lib/state/EventDataSlice";
 import {useAppDispatch} from "@/lib/state/Hooks";
 import {INode} from "@/lib/data/osh/Node";
+import {QrCode} from "@mui/icons-material";
+import QrScanner from "qr-scanner";
+import {CloseIcon} from "next/dist/client/components/react-dev-overlay/internal/icons/CloseIcon";
 
 interface FileWithWebId {
     file: File;
@@ -46,9 +50,6 @@ interface FileWithWebId {
 export default function AdjudicationDetail(props: { event: EventTableData }) {
     const dispatch = useAppDispatch();
 
-    // const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
-    // const [webIdEnabled, setWebIdEnabled] = useState<boolean>(false);
-
     const [uploadedFiles, setUploadedFiles] = useState<FileWithWebId[]>([])
     const [adjudicationCode, setAdjudicationCode] = useState(AdjudicationCodes.codes[0]);
     const [isotope, setIsotope] = useState<string[]>([]);
@@ -57,15 +58,21 @@ export default function AdjudicationDetail(props: { event: EventTableData }) {
     const [vehicleId, setVehicleId] = useState<string>("");
     const [feedback, setFeedback] = useState<string>("");
     const fileInputRef = useRef<HTMLInputElement | null>(null);
+
+    const [openDialog, setOpenDialog] = useState(false);
+    const videoElement = useRef<HTMLVideoElement>(null);
+    const [qrCodeData, setQrCodeData] = useState<string[]>([]);
+    const scanner = useRef<QrScanner>();
+
     const laneMapRef = useContext(DataSourceContext).laneMapRef;
 
     const adjudication = props.event ? new AdjudicationData(new Date().toISOString(), props.event.occupancyCount, props.event.occupancyObsId, props.event.rpmSystemId) : null;
-
     const [adjData, setAdjData] = useState<AdjudicationData>(adjudication);
 
     const [adjSnackMsg, setAdjSnackMsg] = useState('');
     const [colorStatus, setColorStatus] = useState('');
     const [openSnack, setOpenSnack] = useState(false);
+
 
     const [shouldFetchLogs, setShouldFetchLogs] = useState<boolean>(false);
 
@@ -138,6 +145,38 @@ export default function AdjudicationDetail(props: { event: EventTableData }) {
         );
     };
 
+    const handleCloseQrCodeDialog = () => {
+        scanner?.current?.stop();
+        scanner.current = undefined;
+        setOpenDialog(false);
+    }
+
+
+    const handleQrCode = () => {
+        setOpenDialog(true);
+    };
+
+    useEffect(() => {
+        if (openDialog && videoElement?.current && !scanner.current) {
+            const qrOptions = {
+                onDecodeError: (err: any) => console.error("QR Scan Error:", err),
+                preferredCamera: "environment",
+                highlightScanRegion: true,
+            }
+
+            scanner.current = new QrScanner(videoElement.current, (result) => {
+                setQrCodeData(prev => [...prev, result.data]);
+            }, qrOptions);
+
+
+            scanner?.current?.start().catch((err) => {
+                console.error("Error starting scanner")
+                setAdjSnackMsg("Failed to start camera");
+                setOpenSnack(true);
+            });
+        }
+    }, [openDialog]);
+
     const handleFileUpload = (e: ChangeEvent<HTMLInputElement>) => {
         if (e.target.files === null) {
             return;
@@ -166,9 +205,8 @@ export default function AdjudicationDetail(props: { event: EventTableData }) {
     }
 
     const handleIsotopeSelect = (value: string[]) => {
-        let valueString = value.join(', ');
         let tAdjData = adjData;
-        tAdjData.isotopes.push(valueString);
+        tAdjData.isotopes = value;
         setIsotope(value);
         setAdjData(tAdjData);
     }
@@ -314,6 +352,37 @@ export default function AdjudicationDetail(props: { event: EventTableData }) {
 
     }
 
+    async function sendQrCodeUploadRequest(qrData: string, node: INode, adjId: string) {
+        const encoded = btoa(`${node.auth.username}:${node.auth.password}`);
+        const protocol = node.isSecure ? 'https://' : 'http://';
+
+        const endpoint =  `${protocol}${node.address}:${node.port}${node.oshPathRoot}/buckets/adjudication?adjudicationId=${adjId}`
+        const url = new URL(endpoint);
+
+        const options: RequestInit = {
+            method: 'POST',
+            headers: {
+                'Authorization': `Basic ${encoded}`,
+                'Content-Type': 'text/plain'
+            },
+            mode: 'cors',
+            body: qrData
+        }
+        const response = await fetch(url, options);
+
+        if (!response.ok) {
+            console.error("Successfully uploading qr code:", response);
+            setAdjSnackMsg(`Successfully to upload qr code`);
+            setColorStatus('success');
+            setOpenSnack(true);
+            return;
+        }
+
+        setAdjSnackMsg(`Failed to upload qr code`);
+        setColorStatus('error');
+        setOpenSnack(true);
+        // this returns the file name! probs dont need to do anything atm
+    }
 
     async function sendFileUploadRequest(filePaths: FileWithWebId[], node: INode, adjId: string) {
 
@@ -323,10 +392,8 @@ export default function AdjudicationDetail(props: { event: EventTableData }) {
 
 
         for (const fileData of filePaths) {
-
             const endpoint =  `${protocol}${node.address}:${node.port}${node.oshPathRoot}/buckets/adjudication/${fileData.file.name}?adjudicationId=${adjId}&enableWebId=${fileData.webIdEnabled}}`
             const url = new URL(endpoint);
-
 
             const formData = new FormData(); // this should handle the content type and set it properly
             formData.append('file', fileData.file);
@@ -499,6 +566,7 @@ export default function AdjudicationDetail(props: { event: EventTableData }) {
                     </Paper>
                 )}
                 <Stack direction={"row"} spacing={2} justifyContent={"space-between"} alignItems={"center"} width={"100%"}>
+                    <Stack direction={"row"} spacing={2}>
                         <Button
                             component="label"
                             startIcon={<UploadFileRoundedIcon/>}
@@ -525,6 +593,88 @@ export default function AdjudicationDetail(props: { event: EventTableData }) {
                             />
                         </Button>
 
+                        <Button
+                            component="label"
+                            startIcon={<QrCode/>}
+                            sx={{
+                                display: "flex",
+                                alignItems: "center",
+                                width: "auto",
+                                padding: "8px",
+                                borderStyle: "solid",
+                                borderWidth: "1px",
+                                borderRadius: "10px",
+                                borderColor: "info.main",
+                                backgroundColor: "inherit",
+                                color: "info.main"
+                            }}
+                            onClick={handleQrCode}
+                        >
+                             Scan QR Code
+                        </Button>
+
+                        <Dialog
+                            onClose={handleCloseQrCodeDialog}
+                            open={openDialog}
+                            fullWidth
+                            maxWidth="sm"
+                        >
+                            <IconButton
+                                aria-label="close"
+                                onClick={handleCloseQrCodeDialog}
+                                sx={{
+                                    position: 'absolute',
+                                    right: 8,
+                                    top: 8,
+                                }}
+                            >
+                                <CloseIcon />
+                            </IconButton>
+                            <DialogTitle sx={{ textAlign: 'center', pb: 1}}>
+                                QR Code
+                            </DialogTitle>
+                            <Box
+                                sx={{
+                                    display: 'flex',
+                                    flexDirection: 'column',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                    p: 3,
+                                    pt: 1,
+                                }}
+                            >
+                                <Box className='qr-reader'
+                                     sx={{
+                                         width: 400,
+                                         height: 400,
+                                         maxWidth: 400,
+                                         borderRadius: 2,
+                                         overflow: 'hidden',
+                                         backgroundColor: 'black',
+                                         display: 'flex',
+                                         alignItems: 'center',
+                                         justifyContent: 'center',
+                                     }}
+                                >
+                                    <video
+                                        ref={videoElement}
+                                        style={{
+                                            width: "100%",
+                                            height: "100%",
+                                            objectFit: "cover"
+                                    }}
+                                    />
+                                </Box>
+                                <Button
+                                    variant="contained"
+                                    onClick={handleCloseQrCodeDialog}
+                                    sx={{ mt: 2, minWidth: 120 }}
+                                >
+                                    Done
+                                </Button>
+                            </Box>
+                        </Dialog>
+                    </Stack>
                     <Stack direction={"row"} spacing={2}>
                         <SecondaryInspectionSelect
                             secondarySelectVal={secondaryInspection}
