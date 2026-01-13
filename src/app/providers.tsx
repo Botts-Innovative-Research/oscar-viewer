99545291ca11e467d599904053aa5647b54e4a8c
Salwa Jeries <annette.jeries@gmail.com>
2026-01-13 15:03:30 -0600

    Added breakpoint provider

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,31 +1,51 @@
 "use client";
 
-import { Box, ThemeProvider, createTheme, useMediaQuery } from "@mui/material";
-import {ReactNode, Suspense, useMemo} from "react";
-import CssBaseline from '@mui/material/CssBaseline';
+import {
+  Box,
+  ThemeProvider,
+  createTheme,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { ReactNode, Suspense, useMemo } from "react";
+import CssBaseline from "@mui/material/CssBaseline";
 import { getTheme } from "@/app/style/theme";
-import "@/app/style/global.css"
+import "@/app/style/global.css";
 import SuspenseLoad from "./_components/SuspenseLoad";
 
 export default function Providers({ children }: { children: ReactNode }) {
-
   // Get system preference for dark/light mode
   const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
 
   // Implement custom theme based on system preference
   const theme = useMemo(
     () => createTheme(getTheme(prefersDarkMode ? "dark" : "light")),
-    [prefersDarkMode],
+    [prefersDarkMode]
   );
 
   return (
     <Suspense fallback={<SuspenseLoad />}>
       <ThemeProvider theme={theme}>
-        <Box sx={{backgroundColor: "background.default"}}>
-          <CssBaseline />
-          {children}
-        </Box>
+        <CssBaseline />
+        {children}
       </ThemeProvider>
     </Suspense>
   );
+}
+
+// Handle breakpoints for desktop vs. tablets vs. mobile displays
+export const useBreakpoint = () => {
+  const theme = useTheme();
+
+  const isDesktop = useMediaQuery(theme.breakpoints.up("lg"));
+  const isTablet = useMediaQuery(theme.breakpoints.up("md"));
+
+  const size = isDesktop ? "desktop" : isTablet ? "tablet" : "mobile"
+
+  return {
+    size,
+    isMobile: size === "mobile",
+    isTablet: size === "tablet",
+    isDesktop: size === "desktop",
+  };
 }
--- src/app/style/theme.ts
+++ src/app/style/theme.ts
@@ -1,6 +1,8 @@
 import { PaletteMode, ThemeOptions } from "@mui/material";
+import { createTheme, responsiveFontSizes } from "@mui/material/styles";
+
 // When using TypeScript 4.x and above
-import type {} from '@mui/x-data-grid/themeAugmentation';
+import type {} from "@mui/x-data-grid/themeAugmentation";
 
 declare module "@mui/material/styles" {
   interface Palette {
@@ -17,68 +19,81 @@ declare module "@mui/material/styles" {
   }
 }
 
-export const getTheme = (mode: PaletteMode): ThemeOptions => ({
-  palette: {
-    mode,
-    errorHighlight: "#D32F2F4D",
-    secondaryHighlight: "#9C27B04D",
-    infoHighlight: "#2196F34D",
-    successHighlight: '#C1D8C2'
-  },
-  typography: {},
-  components: {
-    MuiPaper: {
-      styleOverrides: {
-        root: ({ ownerState }) => ({
-          ...(ownerState.variant === "outlined" && {
+export const getTheme = (mode: PaletteMode) => {
+  let theme = createTheme({
+    breakpoints: {
+      values: {
+        xs: 0, // Phones
+        sm: 600,
+        md: 900, // Tablets
+        lg: 1200, // Small laptops
+        xl: 1536,
+      },
+    },
+    palette: {
+      mode,
+      errorHighlight: "#D32F2F4D",
+      secondaryHighlight: "#9C27B04D",
+      infoHighlight: "#2196F34D",
+      successHighlight: "#C1D8C2",
+    },
+    typography: {},
+    components: {
+      MuiPaper: {
+        styleOverrides: {
+          root: ({ ownerState }) => ({
+            ...(ownerState.variant === "outlined" && {
+              borderRadius: "10px",
+            }),
+          }),
+        },
+      },
+      MuiButton: {
+        styleOverrides: {
+          root: ({ ownerState }) => ({
             borderRadius: "10px",
           }),
-        }),
+        },
       },
-    },
-    MuiButton: {
-      styleOverrides: {
-        root: ({ ownerState }) => ({
-          borderRadius: "10px",
-        }),
+      MuiDataGrid: {
+        styleOverrides: {
+          root: {},
+        },
       },
-    },
-    MuiDataGrid: {
-      styleOverrides: {
-        root: {
+      MuiTextField: {
+        styleOverrides: {
+          root: ({ ownerState }) => ({
+            "& .MuiInputBase-root": {
+              borderRadius: "10px",
+            },
+          }),
+        },
+      },
+      MuiSelect: {
+        styleOverrides: {
+          root: ({ ownerState }) => ({
+            ...(ownerState.variant === "outlined" && {
+              borderRadius: "10px",
+            }),
+          }),
+        },
+      },
+      MuiInputBase: {
+        styleOverrides: {
+          root: ({ ownerState }) => ({
+            ...(ownerState.type === "file" && {
+              color: "transparent",
+              position: "absolute",
+              "& ::file-selector-button": {
+                display: "none",
+              },
+            }),
+          }),
         },
       },
     },
-    MuiTextField: {
-      styleOverrides: {
-        root: ({ ownerState }) => ({
-          "& .MuiInputBase-root": {
-            borderRadius: "10px",
-          },
-        }),
-      },
-    },
-    MuiSelect: {
-      styleOverrides: {
-        root: ({ ownerState }) => ({
-          ...(ownerState.variant === "outlined" && {
-            borderRadius: "10px",
-          }),
-        }),
-      },
-    },
-    MuiInputBase: {
-      styleOverrides: {
-        root: ({ ownerState }) => ({
-          ...(ownerState.type === "file" && {
-            color: "transparent",
-            position: "absolute",
-            "& ::file-selector-button": {
-              display: 'none',
-            },
-          }),
-        }),
-      },
-    },
-  },
-});
+  });
+
+  theme = responsiveFontSizes(theme);
+  return theme;
+};
