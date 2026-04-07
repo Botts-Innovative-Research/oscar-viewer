c6950b4e326b847c6299129add29e41fd6a09768
Alex Almanza <amavb789@gmail.com>
2026-04-07 17:08:58 -0500

    Fix N42 visualization

--- src/app/_components/n42/N42Detail.tsx
+++ src/app/_components/n42/N42Detail.tsx
@@ -1,13 +1,15 @@
 import {Box, Grid, IconButton, Typography} from "@mui/material";
 import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
 import NavigateNextIcon from "@mui/icons-material/NavigateNext";
-import React, {useContext, useEffect, useState} from "react";
+import React, {useCallback, useContext, useEffect, useState} from "react";
 import {LaneMapEntry} from "@/lib/data/oscar/LaneCollection";
 import {N42_REPORT_DEF} from "@/lib/data/Constants";
 import {EventType} from "osh-js/source/core/event/EventType";
 import {EventTableData} from "@/lib/data/oscar/TableHelpers";
 import {DataSourceContext} from "@/app/contexts/DataSourceContext";
 import N42ChartPlayback from "@/app/_components/n42/N42ChartPlayback";
+import {randomUUID} from "osh-js/source/core/utils/Utils";
+import ObservationFilter from "osh-js/source/core/sweapi/observation/ObservationFilter";
 
 export interface N42Report {
     samplingTime: string;
@@ -27,11 +29,54 @@ interface N42FileData {
     backgroundReports: N42Report[];
 }
 
-export default function N42Detail(props: { event: EventTableData; uploadedFiles: string[] }) {
+export default function N42Detail(props: { event: EventTableData; uploadedFiles?: string[] }) {
     const laneMapRef = useContext(DataSourceContext).laneMapRef;
-
     const [fileDataMap, setFileDataMap] = useState<Map<string, N42FileData>>(new Map());
     const [currentPage, setCurrentPage] = useState(0);
+
+    const fetchN42 = useCallback(async() => {
+        // fetch observations between start and endtime and populate chart u can name the filename just like 1 - x
+        const currentLane = props.event.laneId;
+        const currLaneEntry: LaneMapEntry = laneMapRef.current.get(currentLane);
+
+        const n42Stream = currLaneEntry.findDataStreamByObsProperty(N42_REPORT_DEF);
+        if (!n42Stream) {
+            console.warn("No N42 datastream found for this lane");
+            return;
+        }
+
+        let query = await n42Stream.searchObservations(new ObservationFilter({resultTime: `${props.event.startTime}/${props.event.endTime}`}), 100);
+
+        while (query.hasNext()) {
+            let obsCollection = await query.nextPage();
+
+            for (const obs of obsCollection) {
+                const data = obs.result;
+                if (!data) continue;
+
+                const msgFileName: string = data["fileName"];
+                const foreground: N42Report[] = data["Foreground Reports"] ?? [];
+                const background: N42Report[] = data["Background Reports"] ?? [];
+
+                if (foreground.length === 0 && background.length === 0) continue;
+
+                setFileDataMap(prev => {
+                    const next = new Map(prev);
+                    next.set(msgFileName ?? "none -" + randomUUID(), {
+                        fileName: msgFileName ?? "No file name",
+                        foregroundReports: foreground,
+                        backgroundReports: background,
+                    });
+                    return next;
+                });
+            }
+        }
+
+    }, [props.event]);
+
+    useEffect(() => {
+        if (props.event) fetchN42();
+    }, [props.event, fetchN42]);
 
     useEffect(() => {
         const currentLane = props.event.laneId;
@@ -56,11 +101,14 @@ export default function N42Detail(props: { event: EventTableData; uploadedFiles:
             const data = msg.values?.[0]?.data;
             if (!data) return;
 
+            console.log('data', data);
+
             const msgFileName: string = data["fileName"];
-            const matchedFile = props.uploadedFiles.find(fp => msgFileName?.endsWith(fp));
-            if (!matchedFile) {
-                return;
-            }
+            // const matchedFile = props.uploadedFiles.find(fp => msgFileName?.endsWith(fp));
+            // if (!matchedFile) {
+            //     console.warn("no matched file name")
+            //     return;
+            // }
 
             const foreground: N42Report[] = data["Foreground Reports"] ?? [];
             const background: N42Report[] = data["Background Reports"] ?? [];
@@ -68,8 +116,8 @@ export default function N42Detail(props: { event: EventTableData; uploadedFiles:
             if (foreground.length === 0 && background.length === 0) return;
             setFileDataMap(prev => {
                 const next = new Map(prev);
-                next.set(matchedFile, {
-                    fileName: matchedFile,
+                next.set(msgFileName ?? "none -" + randomUUID(), {
+                    fileName: msgFileName ?? "No file name",
                     foregroundReports: foreground,
                     backgroundReports: background,
                 });
